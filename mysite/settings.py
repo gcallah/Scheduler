872cc36b2d962e54d602ef90c74bc9f1a6753d66
LinuxOptics <olea.alex@gmail.com>
2018-10-21 20:18:05 -0400

    Setup local mail server in Django settings. Feedback form will now provide feedback via local shell (until live mail server is setup). Updated some html pages to be more verbose.

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "mysite", "static", ),
     )
+
+EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
--- scheduler/views.py
+++ scheduler/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from scheduler.forms import FeedbackForm
 from scheduler.forms import ScheduleForm
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template.loader import get_template
+
 
 
 site_hdr = "Course Scheduler"
@@ -14,6 +18,36 @@ def about(request):
 
 def feedback(request):
     form_class = FeedbackForm
+    
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            First_name = request.POST.get('First_name', '')
+            Last_name = request.POST.get('Last_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            comments = request.POST.get('Comments', '')
+
+            template = get_template('feedback_template.txt')
+            context = {
+                'First_name': First_name,
+                'Last_name': Last_name,
+                'Contact_email': contact_email,
+                'Comments': comments,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" +'',
+                ['youremail@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+
+            return redirect('/scheduler/feedback')
+
     return render(request, 'feedback.html', {'form': form_class,})
 
 def requirements(request):
