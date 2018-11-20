d6f24a269d4addc7f66832fccfb4e348b729c61c
gcallah <gcallah@mac.com>
2018-11-20 12:58:03 -0500

    Allow user to set host port.

--- container.sh
+++ container.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+export HOST_PORT="8000"
+if [ $1 ]
+then
+    HOST_PORT=$1
+fi
+
 docker rm scheduler 2> /dev/null || true 
-docker run -it -p 8000:8000 -v $PWD:/home/Scheduler scheduler bash
+docker run -it -p $HOST_PORT:8000 -v $PWD:/home/Scheduler scheduler bash
