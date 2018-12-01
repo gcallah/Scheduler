ff247e56147afa2fae509264eb383bc8cb7345dd
Lucas O'Rourke <lor215@nyu.edu>
2018-12-02 00:34:52 +0100

    Setup two new unit tests

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from scheduler.models import Course, Room
+from scheduler.views import make_schedule, get_unscheduled_course
 
 
 class AlgorithmTestCase(TestCase):
@@ -8,17 +9,17 @@ class AlgorithmTestCase(TestCase):
     # Setup the courses and rooms
     def setUp(self):
         courses = {
-            "Intro": 15,
-            "DevOps": 50,
-            "Algo": 100,
-            "TooBig": 5000
+            "Course15": 15,
+            "Course50": 50,
+            "Course100": 100,
+            "Course5000": 5000
         }
 
         rooms = {
-            "Room1": 20,
-            "Room2": 50,
-            "Room3": 75,
-            "Room4": 150
+            "Room20": 20,
+            "Room50": 50,
+            "Room75": 75,
+            "Room150": 150
         }
         self.username = 'schedulerTests'
         self.password = 'valid_password'
@@ -27,12 +28,32 @@ class AlgorithmTestCase(TestCase):
                                              'fake@email.com',
                                              self.password)
 
-        for key, value in courses:
+        for key in courses.keys():
             Course.objects.create(
                 cname=key,
-                capacity=value)
+                capacity=courses[key])
 
-        for key, value in rooms:
+        for key in rooms:
             Room.objects.create(
                 rname=key,
-                capacity=value)
+                capacity=rooms[key])
+
+    def test_course_with_no_room_available(self):
+
+        all_courses = Course.objects.all()
+        all_rooms = Room.objects.all()
+
+        returned_schedule = make_schedule(all_courses, all_rooms, all_courses)
+        returned_unscheduled = get_unscheduled_course(all_courses, returned_scheduled, all_courses)
+
+        self.assertEqual(returned_unscheduled.size(), 1)
+
+        
+    def test_courses_with_rooms_available_scheduled(self):
+
+        all_courses = Course.objects.filter(capacity<150)
+        all_rooms = Room.objects.all()
+
+        returned_unscheduled = make_schedule(all_courses, all_rooms, all_courses)
+
+        assertEqual(returned_unscheduled.size(), all_courses.size())
