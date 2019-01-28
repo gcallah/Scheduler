4df21a137cd44800d6c4e0b9b45737caa4da5458
gcallah <gcallah@mac.com>
2019-01-28 13:20:57 -0500

    Added new azure host.

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
-                 'aci-demo222.eastus.azurecontainer.io']
+                 'nyuscheduler.azurewebsites.net']
 
 
 # Application definition
