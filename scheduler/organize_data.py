c69d4176ce975e9a5728ba51080ea119ec8576c7
Cee <i@cee.moe>
2019-03-30 22:32:30 -0400

    Remove scheduled from json

--- scheduler/organize_data.py
+++ scheduler/organize_data.py
@@ -10,8 +10,7 @@ def organize(form_data):
         'consumers': organize_courses(courses_from_form, all_courses),
         'resources': {
             'rooms': organize_rooms(all_rooms)
-        },
-        'scheduled': []
+        }
     }
     return data
 
