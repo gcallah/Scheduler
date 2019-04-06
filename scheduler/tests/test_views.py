6c0a7f1bc0095ff8e4fac077679006842c7a3b27
xiaofan <xn291@nyu.edu>
2019-04-06 16:29:52 -0400

    add tests for views

--- scheduler/tests/test_views.py
+++ scheduler/tests/test_views.py
@@ -1,4 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase 
+from unittest import mock
+import json
 
 class ViewsTest(TestCase):
 
@@ -6,3 +8,33 @@ class ViewsTest(TestCase):
         response = self.client.get('/scheduler/index', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_about(self):
+        response = self.client.get('/scheduler/about', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_feedback(self):
+        response = self.client.post('/scheduler/feedback', {})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'feedback.html')
+
+    def mock_organize(self):
+        return {}
+
+    def mock_sched(self):
+        res = {
+            "scheduled": [],
+            "unscheduled": []
+        }
+        return json.dumps(res)
+
+    @mock.patch('scheduler.views.organize', mock_organize)
+    @mock.patch('scheduler.views.sched', mock_sched)
+    def test_schedule(self):
+        response = self.client.post('/scheduler/schedule', {})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'schedule.html')
+
+
+
