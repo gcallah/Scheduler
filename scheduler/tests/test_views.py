4524c9de0ab99b8a3f1f3e4ec969df76b28129b6
xiaofan <xn291@nyu.edu>
2019-04-05 23:59:02 -0400

    fix test for index view

--- scheduler/tests/test_views.py
+++ scheduler/tests/test_views.py
@@ -3,6 +3,6 @@ from django.test import TestCase
 class ViewsTest(TestCase):
 
     def test_index(self):
-        response = self.client.get('/scheduler/index')
+        response = self.client.get('/scheduler/index', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
