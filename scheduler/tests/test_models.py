1e6408fe1fd77ddeb78a343f47ed1ac3d3361d58
xiaofan <xn291@nyu.edu>
2019-04-14 18:56:42 -0400

    add test cases for model Time & Day

--- scheduler/tests/test_models.py
+++ scheduler/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from scheduler.models import Room, Course
+from scheduler.models import Room, Course, Time, Day
 from ddt import ddt, file_data
 import os
 
@@ -29,3 +29,33 @@ class CourseTest(TestCase):
         c = self.create_course(cname)
         self.assertTrue(isinstance(c, Course))
         self.assertEqual(c.__str__(), expected_cname)
+
+@ddt
+class TimeTest(TestCase):
+
+    def create_time(self, times):
+        return Time.objects.create(times=times)
+    
+    @file_data(os.path.join(ROOT_DIR, "test_data/test_time_data.json"))
+    def test_time_creation(self, times, expected_times):
+        t = self.create_time(times)
+        self.assertTrue(isinstance(t, Time))
+        self.assertEqual(t.__str__(), expected_times)
+
+@ddt
+class DayTest(TestCase):
+
+    def create_day(self, days):
+        return Day.objects.create(days=days)
+
+    @file_data(os.path.join(ROOT_DIR, "test_data/test_day_data.json"))
+    def test_day_creation(self, days, expected_days):
+        d = self.create_day(days)
+        self.assertTrue(isinstance(d, Day))
+        self.assertEqual(d.__str__(), expected_days)
+    
+    def test_day_default_creation(self):
+        day = Day.objects.create()
+        self.assertTrue(isinstance(day, Day))
+        self.assertEqual(day.__str__(), "Monday")
+
