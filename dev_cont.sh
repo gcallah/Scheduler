0818f410c1ef5728211abc98b12b4cd2e36386db
gcallah <gcallah@mac.com>
2019-04-25 16:51:57 -0400

    Was running wrong image.

--- dev_cont.sh
+++ dev_cont.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 export HOST_PORT="8000"
+export CONT_NAME="scheduler"
 if [ $1 ]
 then
     HOST_PORT=$1
 fi
 
-docker rm gcallah/nyusched 2> /dev/null || true 
-docker run -it -p $HOST_PORT:8000 -v $PWD:/home/Scheduler gcallah/nyusched bash
+docker rm $CONT_NAME 2> /dev/null || true 
+docker run -it -p $HOST_PORT:8000 -v $PWD:/home/Scheduler $CONT_NAME bash
