72087a3f5e78a98a9d74f4bd5d86d22d2f0b8cda
Jiheng Fan <55456731+JihengFan@users.noreply.github.com>
2019-10-06 20:26:54 -0400

    added testcase for minConflicts for cspsolver.py

--- scheduler/tests/test_cspsovler.py
+++ scheduler/tests/test_cspsovler.py
@@ -24,5 +24,24 @@ class Cspsolve_TestCase(TestCase):
         self.assertTrue(old_len == len(self.sech.nodes))
         self.assertTrue(self.sech.nodeDomains["class1"] == "domain1")
 
+
+class MinConflicts_TestCase(TestCase):
+    def setUp(self):
+        csp = CSP()
+        self.minC = minConflicts(csp)
+
+    def tearDown(self):
+        self.minC.csp = None
+        self.minC = None
+
+    def test_assigner(self):
+        self.minC.csp.add_node("class1", ["domain1"])
+        self.minC.csp.add_node("class2", ["domain2", "domain3", "domain4"])
+        self.minC.csp.add_node("class3", ["domain5", "domain6"])
+        self.assertTrue(self.minC.initial_var_assignment()["class1"] == "domain1")
+        self.assertTrue(self.minC.initial_var_assignment()["class2"] in ["domain2", "domain3", "domain4"])
+        self.assertFalse(self.minC.initial_var_assignment()["class3"] == "domain10")
+
+
 if __name__ == '__main__':
             main()
