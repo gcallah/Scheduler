acee940d5ee6525a15d17820e180cffc61675fa2
Jiheng Fan <55456731+JihengFan@users.noreply.github.com>
2019-10-07 20:24:48 -0400

    Made a few change to the testcases

--- scheduler/tests/test_cspsovler.py
+++ scheduler/tests/test_cspsovler.py
@@ -6,28 +6,37 @@ from unittest import TestCase, main, skip
 
 from cspsolver import CSP, minConflicts
 
-class Cspsolve_TestCase(TestCase):
+
+def create_csp():
+    csp = CSP()
+    csp.add_node("class1", ["domain1"])
+    csp.add_node("class2", ["domain2", "domain3", "domain4"])
+    csp.add_node("class3", ["domain5", "domain6"])
+    return csp
+
+
+class CSP_TestCase(TestCase):
     def setUp(self):
-        self.sech = CSP()
+        self.csp = CSP()
 
     def tearDown(self):
-        self.sech = None
+        self.csp = None
 
     def test_add(self):
         """
         Test if add node to CSP work.
         """
-        self.sech.add_node("class1", "domain1")
-        self.assertTrue(len(self.sech.nodes) == 1)
-        old_len = len(self.sech.nodes)
-        self.assertFalse(self.sech.add_node("class1","domain1"))
-        self.assertTrue(old_len == len(self.sech.nodes))
-        self.assertTrue(self.sech.nodeDomains["class1"] == "domain1")
+        self.csp.add_node("class1", "domain1")
+        self.assertTrue(len(self.csp.nodes) == 1)
+        old_len = len(self.csp.nodes)
+        self.assertFalse(self.csp.add_node("class1", "domain1"))
+        self.assertTrue(old_len == len(self.csp.nodes))
+        self.assertTrue(self.csp.nodeDomains["class1"] == "domain1")
 
 
 class MinConflicts_TestCase(TestCase):
     def setUp(self):
-        csp = CSP()
+        csp = create_csp()
         self.minC = minConflicts(csp)
 
     def tearDown(self):
@@ -35,13 +44,10 @@ class MinConflicts_TestCase(TestCase):
         self.minC = None
 
     def test_assigner(self):
-        self.minC.csp.add_node("class1", ["domain1"])
-        self.minC.csp.add_node("class2", ["domain2", "domain3", "domain4"])
-        self.minC.csp.add_node("class3", ["domain5", "domain6"])
         self.assertTrue(self.minC.initial_var_assignment()["class1"] == "domain1")
         self.assertTrue(self.minC.initial_var_assignment()["class2"] in ["domain2", "domain3", "domain4"])
         self.assertFalse(self.minC.initial_var_assignment()["class3"] == "domain10")
 
 
 if __name__ == '__main__':
-            main()
+    main()
