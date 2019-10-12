e51f428031826e96119253a27ef076c95f445023
Dolly <devashryee@nyu.edu>
2019-10-12 14:07:42 -0400

    Fixed all flake8 errors in file

--- scheduler/my_data_loader.py
+++ scheduler/my_data_loader.py
@@ -11,22 +11,22 @@ from teachercourse_csp import assigner
 'Part I: Load and manage the spreadsheet data'
 df = pd.read_excel('my_data.xlsx')
 
-prof_df = df.iloc[:,0:4]
-prof_df['prof_courses'] = prof_df['prof_courses'].str.replace(', ',',')
+prof_df = df.iloc[:, 0:4]
+prof_df['prof_courses'] = prof_df['prof_courses'].str.replace(', ', ',')
 prof_df.dropna(inplace=True)
-rooms_df = df.iloc[:,4:6]
+rooms_df = df.iloc[:, 4:6]
 rooms_df.dropna(inplace=True)
 rooms_df['room_capacity'] = rooms_df['room_capacity'].astype(u'int8')
 rooms_df['rooms'] = rooms_df['rooms'].astype('str')
-courses_df = df.iloc[:,6:]
+courses_df = df.iloc[:, 6:]
 courses_df.dropna(inplace=True)
 
-professors = []
+profs = []
 prof_info = {}
 for index, row in prof_df.iterrows():
     data = {}
     prof = row['professor']
-    professors.append(prof)
+    profs.append(prof)
     courses = row['prof_courses'].split(',')
     data['courses'] = courses
     data['start_time'] = row['prof_start_time']
@@ -43,13 +43,13 @@ for _, row in rooms_df.iterrows():
 courses = []
 course_no_students = {}
 course_mins = {}
-course_days_weekly = {}
+course_days = {}
 for _, row in courses_df.iterrows():
     course = row['course']
     courses.append(course)
     course_no_students[course] = int(row['course_no_students'])
     course_mins[course] = int(row['course_mins'])
-    course_days_weekly[course] = int(row['course_days_weekly'])
+    course_days[course] = int(row['course_days_weekly'])
 
 # User feedback
 print("Your excel data is now ready to use")
@@ -58,10 +58,13 @@ print("Your excel data is now ready to use")
 
 
 def user_data_printer():
-    table = {'PROFS': professors,
-             'ROOMS':rooms,
-             'COURSES':courses}
-    my_data = tabulate.tabulate(table, headers=['profs','rooms','courses'],tablefmt='html',colalign=("center","center","center"))
+    table = {'PROFS': profs,
+             'ROOMS': rooms,
+             'COURSES': courses}
+    headers = ['profs', 'rooms', 'courses']
+    tablefmt = 'html'
+    colalign = ("center", "center", "center")
+    my_data = tabulate.tabulate(table, headers, tablefmt, colalign)
     return HTML(my_data)
 
 
@@ -71,29 +74,35 @@ check_your_data = user_data_printer()
 'Part III: Run the scheduler and display output'
 
 
-def time_formatter(course,start_time):
-    hs,ms = start_time
+def time_formatter(course, start_time):
+    hs, ms = start_time
     end = hs*6 + ms//10 + course_mins[course]//10
-    end_time = (end//6,(end - (end//6)*6)*10)
-    he,me = end_time
-    output = '{:0>2}'.format(hs) + ':' + '{:0>2}'.format(ms) + ' to ' + '{:0>2}'.format(he) + ':' + '{:0>2}'.format(me)
+    end_time = (end//6, (end - (end//6)*6)*10)
+    he, me = end_time
+    output = '{:0>2}'.format(hs) + ':' + '{:0>2}'.format(ms)
+    output = output + ' to ' + '{:0>2}'.format(he) + ':' + '{:0>2}'.format(me)
     return output
 
 
-user_data = (professors,prof_info,rooms,room_capacities,courses,course_no_students,course_mins,course_days_weekly)
+user_data = (profs, prof_info, rooms, room_capacities, courses,
+             course_no_students, course_mins, course_days)
 full_schedule = assigner(user_data)
-weekdays = ['mon','tues','wed','thur','fri']
+weekdays = ['mon', 'tues', 'wed', 'thur', 'fri']
 
-columns = ['Day','Course','Professor', 'Room','Period']
-df_out = pd.DataFrame(None,columns=columns)
+columns = ['Day', 'Course', 'Professor', 'Room', 'Period']
+df_out = pd.DataFrame(None, columns=columns)
 for day in weekdays:
     schedule = full_schedule[day]
-    for var,val in schedule.items():
-        course,professor = var
-        room,start_time = val
-        df_inc = {'Day':day.upper(),'Course':[course],'Professor':[professor], 'Room':[room],'Period':[time_formatter(course,start_time)]}
+    for var, val in schedule.items():
+        course, professor = var
+        room, start_time = val
+        df_inc = {'Day': day.upper(),
+                  'Course': [course],
+                  'Professor': [professor],
+                  'Room': [room],
+                  'Period': [time_formatter(course, start_time)]}
         df_inc = pd.DataFrame.from_dict(df_inc)
-        df_out = pd.concat([df_out,df_inc],ignore_index=True)
+        df_out = pd.concat([df_out, df_inc], ignore_index=True)
 
 # show_me_the_schedule = lambda : display(df_out)
 
